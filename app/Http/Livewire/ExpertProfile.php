39b65db08e64f2f79b9d2a46451b3542d77a58b6
artspider <arturo.itiguala@gmail.com>
2020-09-12 11:39:11 -0500

    camba a minusculas los nombres los modelos

--- app/Http/Livewire/ExpertProfile.php
+++ app/Http/Livewire/ExpertProfile.php
@@ -4,8 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
-use App\Expert;
-use App\Tag;
+use App\expert;
+use App\tag;
 use App\expert_tag;
 use App\titulo;
 use App\trabajo;
